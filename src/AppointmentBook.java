32e2d979747d2dcf9c910aa0c2eadbc2d098be21
LailianaMuniz <lailianam@nycstudents.net>
2025-09-26 11:45:34 -0400

    added loop and made new appointmentbook in main

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -19,7 +19,20 @@ public class AppointmentBook
     }
     public void printPeriod(int period)
     {
-        for(int i = 0; i < schedule[period - 1].length; i++);
-        System.out.println(i + " " + schedule[period - 1][i]);
+        for(int i = 0; i < schedule[period - 1].length; i++)
+            System.out.println(i + " " + schedule[period - 1][i]);
+    }
+    public int findFreeBlock(int period, int duration)
+    {
+        int block = 0;
+        for(int i = 0; i < 60; i++)
+            if(isMinuteFree(period, i))
+            {
+                block++;
+                if(block == duration)
+                    return i - duration + 1;
+                else block = 0;
+                return -1;
+            }
     }
 }
