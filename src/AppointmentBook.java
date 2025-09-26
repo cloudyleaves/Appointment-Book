c13317820d2c364cd4bf2039f29cd2749d5d55d3
LailianaMuniz <lailianam@nycstudents.net>
2025-09-26 11:27:16 -0400

    added code, will go back to fix the error

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -10,3 +10,16 @@ private boolean isMinuteFree(int period, int minute)
 {
     return schedule[period - 1][minute];
 }
+public int findFreeBlock(int period, int duration)
+{
+    return 0;
+}
+public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
+{
+    return false;
+}
+public void printPeriod(int period)
+{
+    for(int i = 0; i < schedule[period - 1].length; i++);
+    System.out.println(i + " " + schedule[period - 1][i]);
+}
